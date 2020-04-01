b1c05ea96c808e4bbda0fe53ca37d67c0c65fae8
ThizThizzyDizzy <>
2020-04-01 12:46:22 -0400

    The pre-overhaul generator no longer has air selected by default Darkened the background of deselected parts Added bzy-xyz generation model for pre-overhaul reactors Added iteration counter Fixed crash when attempting to export null reactors Imported reactor details boxes are no longer editable Fixed merge-timeout not always applying with multithreading Fixed crash when changing reactor size after importing a reactor Added "Apply Symmetry" option Fixed pre-overhaul copper using overhaul texture fixed pre-overhaul coolers being called Heatsinks Added Export image and JSON options to the import reactor window

--- src/common/Version.java
+++ src/common/Version.java
@@ -1,5 +1,5 @@
 package common;
 public class Version{
     //Did I need a class to store the version number? no. Did I do it anyway? yes.
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1.0";
 }
