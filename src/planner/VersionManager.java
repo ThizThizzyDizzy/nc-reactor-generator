038fcd697a9a3c409d3cc161997570bce5dfcd1c
ThizThizzyDizzy <thizthizzydizzy@gmail.com>
2020-06-30 22:52:01 -0400

    Incremented version number

--- src/planner/VersionManager.java
+++ src/planner/VersionManager.java
@@ -25,6 +25,7 @@ public class VersionManager{
         addVersion("2.0.1");
         addVersion("2.0.2");
         addVersion("2.0.3");
+        addVersion("2.1.0");
         currentVersion = versions.get(versions.size()-1);
     }
     /**
