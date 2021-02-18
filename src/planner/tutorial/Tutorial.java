f5c0b2aca66d4473609a896cd9bbe095821dec61
ThizThizzyDizzy <thizthizzydizzy@gmail.com>
2021-02-17 21:08:19 -0500

    Made font characters %*'" a little nicer Added tutorials for underhaul SFRs, as well as overhaul SFRs, MSRs, and Turbines

--- src/planner/tutorial/Tutorial.java
+++ src/planner/tutorial/Tutorial.java
@@ -1,19 +1,10 @@
 package planner.tutorial;
-import java.awt.image.BufferedImage;
 import java.util.ArrayList;
-import java.util.Random;
-import multiblock.action.SetSelectionAction;
-import multiblock.action.SetblocksAction;
-import org.lwjgl.opengl.GL11;
-import planner.Core;
-import planner.menu.MenuEdit;
-import planner.menu.component.editor.MenuComponentEditorGrid;
-import simplelibrary.opengl.ImageStash;
 import simplelibrary.opengl.Renderer2D;
 public abstract class Tutorial extends Renderer2D{
-    public static ArrayList<TutorialCategory> categories = new ArrayList<>();
-//    private static final TutorialCategory underhaul;
-//    private static final TutorialCategory overhaul;
+    public static final ArrayList<TutorialCategory> categories = new ArrayList<>();
+    private static final TutorialCategory underhaul;
+    private static final TutorialCategory overhaul;
     private static final TutorialCategory planner;
     public final String name;
     public boolean live = false;
@@ -22,55 +13,14 @@ public abstract class Tutorial extends Renderer2D{
         planner.add(TutorialFileReader.read("tutorials/planner/introduction.ncpt"));
         planner.add(TutorialFileReader.read("tutorials/planner/basics.ncpt"));
         planner.add(TutorialFileReader.read("tutorials/planner/editing.ncpt"));
-//        categories.add(underhaul = new TutorialCategory("Underhaul"));
-//        categories.add(overhaul = new TutorialCategory("Overhaul"));
+        categories.add(underhaul = new TutorialCategory("Underhaul"));
+        categories.add(overhaul = new TutorialCategory("Overhaul"));
+        underhaul.add(TutorialFileReader.read("tutorials/underhaul/sfr.ncpt"));
+        overhaul.add(TutorialFileReader.read("tutorials/overhaul/sfr.ncpt"));
+        overhaul.add(TutorialFileReader.read("tutorials/overhaul/msr.ncpt"));
+        overhaul.add(TutorialFileReader.read("tutorials/overhaul/turbine.ncpt"));
         //TODO tutorial on modifying configs and whatnot (first redo config system
         //TODO tutorial on using the generator
-//        underhaul.add(new Tutorial("SFRs"){
-//            @Override
-//            public double getHeight(){
-//                return .1;
-//            }
-//            @Override
-//            public void render(float resonatingBrightness, float frame){
-//            }
-//        });
-//        overhaul.add(new Tutorial("Fission"){
-//            @Override
-//            public double getHeight(double width){
-//                return width*.1;
-//            }
-//            @Override
-//            public void render(float resonatingBrightness, float frame){
-//            }
-//        });
-//        overhaul.add(new Tutorial("SFRs"){
-//            @Override
-//            public double getHeight(double width){
-//                return width*.1;
-//            }
-//            @Override
-//            public void render(float resonatingBrightness, float frame){
-//            }
-//        });
-//        overhaul.add(new Tutorial("MSRs"){
-//            @Override
-//            public double getHeight(double width){
-//                return width*.1;
-//            }
-//            @Override
-//            public void render(float resonatingBrightness, float frame){
-//            }
-//        });
-//        overhaul.add(new Tutorial("Turbines"){
-//            @Override
-//            public double getHeight(double width){
-//                return width*.1;
-//            }
-//            @Override
-//            public void render(float resonatingBrightness, float frame){
-//            }
-//        });
     }
     public Tutorial(String name){
         this.name = name;
