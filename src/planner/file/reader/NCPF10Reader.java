2605bdd5f0cedbcb23c949cb5f49d67b422df4cc
Alissa Rao <lymia@lymiahugs.com>
2021-06-30 03:43:19 -0700

    Reader: Continue work on simplifying configuration parser code.

--- src/planner/file/reader/NCPF10Reader.java
+++ src/planner/file/reader/NCPF10Reader.java
@@ -3,6 +3,9 @@ package planner.file.reader;
 import simplelibrary.config2.Config;
 
 public class NCPF10Reader extends NCPF11Reader {
+    protected byte getTargetVersion() {
+        return (byte) 10;
+    }
     protected int readRuleBlockIndex(Config config, String name) {
         return (byte) config.get(name);
     }
