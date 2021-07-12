1470519bc8f4afa7230c081f9983deae032bb128
thizthizzydizzy <thizthizzydizzy@gmail.com>
2021-07-12 07:53:13 -0400

    Incremented minor version

--- src/planner/VersionManager.java
+++ src/planner/VersionManager.java
@@ -103,6 +103,7 @@ public class VersionManager{
         addVersion("3.0.8");
         addVersion("3.1");
         addVersion("3.2");
+        addVersion("3.3");
         currentVersion = versions.get(versions.size()-1);
     }
     /**
