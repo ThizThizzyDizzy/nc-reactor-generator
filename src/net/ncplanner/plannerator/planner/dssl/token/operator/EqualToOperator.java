4f8bc64d6c711fe6401df128b8fdb629492e6e63
ThizThizzyDizzy <thizthizzydizzy@gmail.com>
2022-12-12 09:33:51 -0500

    Fixed == operator crashing with nulls

--- src/net/ncplanner/plannerator/planner/dssl/token/operator/EqualToOperator.java
+++ src/net/ncplanner/plannerator/planner/dssl/token/operator/EqualToOperator.java
@@ -1,6 +1,7 @@
 package net.ncplanner.plannerator.planner.dssl.token.operator;
 import net.ncplanner.plannerator.planner.dssl.Script;
 import net.ncplanner.plannerator.planner.dssl.object.StackBool;
+import net.ncplanner.plannerator.planner.dssl.object.StackNull;
 import net.ncplanner.plannerator.planner.dssl.object.StackObject;
 public class EqualToOperator extends Operator{
     public EqualToOperator(){
@@ -12,6 +13,7 @@ public class EqualToOperator extends Operator{
     }
     @Override
     public StackObject evaluate(Script script, StackObject v1, StackObject v2){
+        if(v1==StackNull.INSTANCE||v2==StackNull.INSTANCE)return new StackBool(v2==v1);
         return new StackBool(v1.getBaseValue().equals(v2.getBaseValue()));
     }
 }
