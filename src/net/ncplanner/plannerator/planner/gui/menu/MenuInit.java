4df7f7a466935cbf1e28c8f935783717f9125c05
Thiz <Thizthizzydizzy@gmail.com>
2024-05-24 12:40:36 -0400

    Fixed all modules being enabled upon loading settings (#161)

--- src/net/ncplanner/plannerator/planner/gui/menu/MenuInit.java
+++ src/net/ncplanner/plannerator/planner/gui/menu/MenuInit.java
@@ -193,7 +193,6 @@ public class MenuInit extends Menu{
                         }
                     }
                     for(Module m : Core.modules){
-                        moduleStates.put(m, true);
                         if(!moduleStates.containsKey(m))continue;
                         if(m.isActive()){
                             if(!moduleStates.get(m))m.deactivate(false);
