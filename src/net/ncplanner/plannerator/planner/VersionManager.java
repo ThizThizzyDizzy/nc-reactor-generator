510a6cbbb281313b00288798032e468be56302a6
Thiz <Thizthizzydizzy@gmail.com>
2024-05-24 20:48:24 -0400

    bumped version to 5.0.0-beta.3

--- src/net/ncplanner/plannerator/planner/VersionManager.java
+++ src/net/ncplanner/plannerator/planner/VersionManager.java
@@ -122,7 +122,7 @@ public class VersionManager{
         addVersion("4.3");
         addVersion("4.4");
         addVersion("4.5");
-        addVersion("5.0.0-beta.2");
+        addVersion("5.0.0-beta.3");
         currentVersion = versions.get(versions.size()-1);
     }
     /**
