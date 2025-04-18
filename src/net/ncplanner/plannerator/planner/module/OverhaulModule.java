f7c848452778bb1ae470abec5579d7e4d5086c52
Thiz <Thizthizzydizzy@gmail.com>
2025-04-18 19:45:38 -0400

    Converted NCO Confectionery to ncpf.json

--- src/net/ncplanner/plannerator/planner/module/OverhaulModule.java
+++ src/net/ncplanner/plannerator/planner/module/OverhaulModule.java
@@ -150,12 +150,13 @@ public class OverhaulModule extends Module<Object>{
         addAddonTask(t, "Binary's Extra Stuff", "configurations/addons/binarys_extra_stuff.ncpf.json", "https://www.curseforge.com/minecraft/customization/binarys-extra-stuff-bes");
         addAddonTask(t, "Alternative Ore Processing", "configurations/addons/alternative_ore_processing.ncpf.json", "https://www.curseforge.com/minecraft/customization/aop");
         addAddonTask(t, "Crazy Ore Processing", "configurations/addons/crazy_ore_processing.ncpf.json", "https://www.curseforge.com/minecraft/customization/crazy-ore-processing");
-        addLegacyAddonTask(t, "NCO Confectionery", "configurations/addons/nco_confectionery.ncpf", "https://www.curseforge.com/minecraft/customization/nco-confectionery");
+        addAddonTask(t, "NCO Confectionery", "configurations/addons/nco_confectionery.ncpf.json", "https://www.curseforge.com/minecraft/customization/nco-confectionery");
         addLegacyAddonTask(t, "Thorium Mixed Fuels", "configurations/addons/thorium_mixed_fuels.ncpf", "https://www.curseforge.com/minecraft/customization/thorium-mixed-fuels");
         addLegacyAddonTask(t, "Inert Matrix Fuels", "configurations/addons/inert_matrix_fuels.ncpf", "https://www.curseforge.com/minecraft/customization/inert-matrix-fuels");
         addLegacyAddonTask(t, "Alloy Heat Sinks", "configurations/addons/alloy_heat_sinks.ncpf", "https://www.curseforge.com/minecraft/customization/alloy-heat-sinks");
         addLegacyAddonTask(t, "Spicy Heat Sinks (Stable)", "configurations/addons/spicy_heat_sinks_stable.ncpf", "https://www.curseforge.com/minecraft/customization/spicy-heat-sinks");
         addLegacyAddonTask(t, "Spicy Heat Sinks (Unstable)", "configurations/addons/spicy_heat_sinks_unstable.ncpf", "https://www.curseforge.com/minecraft/customization/spicy-heat-sinks");
+        //TODO (don't forget to update the credits!)
         runTasks();
     }
     private final EditorOverlay<net.ncplanner.plannerator.multiblock.overhaul.fissionsfr.Block> sfrActiveModeratorOverlay = new EditorOverlay<net.ncplanner.plannerator.multiblock.overhaul.fissionsfr.Block>("Active Moderators", "Highlights active moderators with a green outline", true){
