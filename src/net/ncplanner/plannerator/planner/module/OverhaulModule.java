f704d2f6a6da6674e1726d773effddcdb047acbe
Thiz <Thizthizzydizzy@gmail.com>
2025-04-25 16:30:51 -0400

    Updated Inert Matrix Fuels to ncpf.json

--- src/net/ncplanner/plannerator/planner/module/OverhaulModule.java
+++ src/net/ncplanner/plannerator/planner/module/OverhaulModule.java
@@ -152,7 +152,7 @@ public class OverhaulModule extends Module<Object>{
         addAddonTask(t, "Crazy Ore Processing", "configurations/addons/crazy_ore_processing.ncpf.json", "https://www.curseforge.com/minecraft/customization/crazy-ore-processing");
         addAddonTask(t, "NCO Confectionery", "configurations/addons/nco_confectionery.ncpf.json", "https://www.curseforge.com/minecraft/customization/nco-confectionery");
         addAddonTask(t, "Thorium Mixed Fuels", "configurations/addons/thorium_mixed_fuels.ncpf.json", "https://www.curseforge.com/minecraft/customization/thorium-mixed-fuels");
-        addLegacyAddonTask(t, "Inert Matrix Fuels", "configurations/addons/inert_matrix_fuels.ncpf", "https://www.curseforge.com/minecraft/customization/inert-matrix-fuels");
+        addAddonTask(t, "Inert Matrix Fuels", "configurations/addons/inert_matrix_fuels.ncpf.json", "https://www.curseforge.com/minecraft/customization/inert-matrix-fuels");
         addLegacyAddonTask(t, "Alloy Heat Sinks", "configurations/addons/alloy_heat_sinks.ncpf", "https://www.curseforge.com/minecraft/customization/alloy-heat-sinks");
         addLegacyAddonTask(t, "Spicy Heat Sinks (Stable)", "configurations/addons/spicy_heat_sinks_stable.ncpf", "https://www.curseforge.com/minecraft/customization/spicy-heat-sinks");
         addLegacyAddonTask(t, "Spicy Heat Sinks (Unstable)", "configurations/addons/spicy_heat_sinks_unstable.ncpf", "https://www.curseforge.com/minecraft/customization/spicy-heat-sinks");
